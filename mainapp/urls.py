6c3d3793889fbdcbc7639615a901e4383112b98b
vladislav <vladislavchicheg@gmail.com>
2023-01-05 17:45:46 +0400

    Lesson_2 add static pages

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,6 +8,10 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.HelloWorldView.as_view()),
-    path("<str:word>/", views.check_kwargs),
+    path("", views.MainPageView.as_view()),
+    path("news/", views.NewsPageView.as_view()),
+    path("courses_list/", views.CoursesPageView.as_view()),
+    path("contacts/", views.ContactsPageView.as_view()),
+    path("doc_site/", views.DocSitePageView.as_view()),
+    path("login/", views.LoginPageView.as_view()),
 ]
